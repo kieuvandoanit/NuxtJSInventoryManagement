3cbdf126b54b696a225318b2959a5a40ab166e4c
Doan Kieu <kieuvandoanit@gmail.com>
2024-07-27 14:27:04 +0700

    second commit

--- app.vue
+++ app.vue
@@ -27,21 +27,3 @@
     </NuxtErrorBoundary>
   </NuxtLayout>
 </template>
-
-<!-- // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC-tR7Jh0770m3uNztc8KCvvpZ2P3uDnxk",
-  authDomain: "starter-realtime-database.firebaseapp.com",
-  projectId: "starter-realtime-database",
-  storageBucket: "starter-realtime-database.appspot.com",
-  messagingSenderId: "16117444026",
-  appId: "1:16117444026:web:c2a862fa21648239860183"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig); -->
